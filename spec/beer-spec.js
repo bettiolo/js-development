310850b6a806b42a76d465b005b2f59022ff0261
Marco Bettiolo <marco@bettiolo.it>
2013-10-08 00:37:12 +0100

    Refactored for better inheritance and used properties

--- spec/beer-spec.js
+++ spec/beer-spec.js
@@ -1,12 +1,12 @@
-describe("Glass", function() {
+describe("A new glass", function() {
 	var glass;
 
 	beforeEach(function() {
 		glass = new Glass();
 	});
 
-	it("It is empty", function() {
-		expect(glass.volume()).toEqual(0);
+	it("is empty", function() {
+		expect(glass.volume).toEqual(0);
 	});
 });
 
@@ -18,10 +18,10 @@ describe('Pint', function() {
 	});
 
 	it('A new pint should be 20 fl. oz.', function () {
-		expect(pint.volume()).toEqual(20);
+		expect(pint.volume).toEqual(20);
 	});
 	it('The glass should be 20 fl. oz. in max volume', function () {
-		expect(pint.maxVolume()).toEqual(20);
+		expect(pint.maxVolume).toEqual(20);
 	});
 });
 
@@ -33,11 +33,11 @@ describe('HalfPint', function() {
 	});
 
 	it('A new half pint should be 10 fl. oz.', function () {
-		expect(halfPint.volume()).toEqual(10);
+		expect(halfPint.volume).toEqual(10);
 	});
 
 	it('The glass should be 10 fl. oz. in max volume', function () {
-		expect(halfPint.maxVolume()).toEqual(10);
+		expect(halfPint.maxVolume).toEqual(10);
 	});
 });
 
@@ -50,7 +50,7 @@ describe('Jug', function () {
 
 	it('It is served as 3 pints (60 fl. oz.)', function () {
 
-		expect(jug.maxVolume()).toEqual(60);
+		expect(jug.maxVolume).toEqual(60);
 	});
 
 });
@@ -67,17 +67,17 @@ describe("Customer", function() {
 	it("Every time the customer drinks, 1 fluid ounce is consumed", function() {
 		customer.drink(pint);
 
-		expect(pint.volume()).toEqual(19);
+		expect(pint.volume).toEqual(19);
 	});
 
 	it("Every time the customer quaffs, 4 fluid ounces are consumed", function() {
 		customer.quaff(pint);
-		expect(pint.volume()).toEqual(16);
+		expect(pint.volume).toEqual(16);
 	});
 
 	it("Every time customer downs in one, the entire remaining contents of the glass are consumed", function(){
 		customer.downInOne(pint);
-		expect(pint.volume()).toEqual(0);
+		expect(pint.volume).toEqual(0);
 	});
 });
 
--- src/beer.js
+++ src/beer.js
@@ -1,20 +1,12 @@
 var Glass = function () {
 
 	function Glass(maxVolume) {
-		this._maxVolume = maxVolume;
-		this._volume = 0;
+		this.volume = 0;
+		this.maxVolume = maxVolume;
 	}
 
-	Glass.prototype = {
-		maxVolume : function () {
-			return this._maxVolume;
-		},
-		volume : function () {
-			return this._volume;
-		},
-		consume : function (quantityInOz) {
-			this._volume -= quantityInOz;
-		}
+	Glass.prototype.consume = function (quantityInOz) {
+		this.volume -= quantityInOz;
 	};
 
 	return Glass;
@@ -23,10 +15,12 @@ var Glass = function () {
 var Pint = function () {
 
 	function Pint() {
-		this._volume = 20;
+		Glass.call(this, 20);
+		this.volume = 20;
 	}
 
-	Pint.prototype = new Glass(20);
+	Pint.prototype = new Glass();
+	Pint.prototype.constructor = Pint;
 
 	return Pint;
 }();
@@ -34,10 +28,12 @@ var Pint = function () {
 var HalfPint = function () {
 
 	function HalfPint() {
-		this._volume = 10;
+		Glass.call(this, 10)
+		this.volume = 10;
 	}
 
-	HalfPint.prototype = new Glass(10);
+	HalfPint.prototype = new Glass();
+	HalfPint.prototype.constructor = HalfPint;
 
 	return HalfPint;
 }();
@@ -45,10 +41,11 @@ var HalfPint = function () {
 var Jug = function(){
 
 	function Jug(){
-
+		Glass.call(this, 60);
 	}
 
-	Jug.prototype = new Glass(60);
+	Jug.prototype = new Glass();
+	Jug.prototype.constructor = Jug;
 
 	return Jug;
 }();
