5b42c3637bfd0f0d6e3975e4f8ce4425fc32e9f6
Marco Bettiolo <marco@bettiolo.it>
2013-10-25 02:41:29 +0100

    Aliased namespace and pointed the config to the root

--- test/spec-runner.config.js
+++ test/spec-runner.config.js
@@ -1,15 +1,16 @@
 /*global require */
 
 require.config({
-    baseUrl: '../js',
-    // urlArgs: 'cb=' + Math.random(),
-    paths: {
-        'jasmine' : '../lib/jasmine-1.3.0/jasmine',
-        'jasmine-html' : '../lib/jasmine-1.3.0/jasmine-html',
-        'domReady' : '../lib/domReady-2.0.1/domReady',
-        'spec-runner' : '../test/spec-runner'
+    baseUrl : '../',
+    // urlArgs : 'cb=' + Math.random(),
+    paths : {
+        'pub' : 'js/pub',
+        'jasmine' : 'lib/jasmine-1.3.0/jasmine',
+        'jasmine-html' : 'lib/jasmine-1.3.0/jasmine-html',
+        'domReady' : 'lib/domReady-2.0.1/domReady',
+        'spec-runner' : 'test/spec-runner'
     },
-    shim: {
+    shim : {
         'jasmine-html' : {
             deps : ['jasmine'],
             exports : 'jasmine'
