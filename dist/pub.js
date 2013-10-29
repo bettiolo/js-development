dc73eebc2f8e68863ed8c2cf949f4405a62d8fab
Marco Bettiolo <marco@bettiolo.it>
2013-10-29 01:06:24 +0000

    added more tests

--- dist/pub.js
+++ dist/pub.js
@@ -423,121 +423,123 @@ define("lib/almond.js", function(){});
 /*global define:true */
 
 define('pub/glass',[], function () {
-    
+	
 
-    /**
-     * @constructor
-     * @param {number} capacity The capacity of the glass in oz.
-     * @abstract
-     */
-    function Glass(capacity) {
-        this.quantity = 0;
-        this.capacity = capacity;
-    }
+	/**
+	 * @constructor
+	 * @param {number} capacity The capacity of the glass in oz.
+	 * @abstract
+	 */
+	function Glass(capacity) {
+		this.quantity = 0;
+		this.capacity = capacity;
+	}
 
-    Glass.prototype.constructor = Glass;
+	Glass.prototype.constructor = Glass;
 
-    /** @param {number} consumedQuantity How much has been consumed in oz. */
-    Glass.prototype.consume = function (consumedQuantity) {
-        this.quantity -= consumedQuantity;
-    };
+	/** @param {number} consumedQuantity How much has been consumed in oz. */
+	Glass.prototype.consume = function (consumedQuantity) {
+		if (this.quantity > 0) {
+			this.quantity -= consumedQuantity;
+		}
+	};
 
-    /**
-     * Enumerator for the beer volume in oz.
-     * @constant
-     * @readonly
-     * @enum {number}
-     */
-    Glass.Size = {
-        PINT : 20,
-        HALF_PINT : 10,
-        JUG : 60
-    }; // should use Object.freeze
+	/**
+	 * Enumerator for the beer volume in oz.
+	 * @constant
+	 * @readonly
+	 * @enum {number}
+	 */
+	Glass.Size = {
+		PINT : 20,
+		HALF_PINT : 10,
+		JUG : 60
+	}; // should use Object.freeze
 
-    return Glass;
+	return Glass;
 });
 /*global define */
 
 define('pub/pint',['pub/glass'], function (Glass) {
-    
+	
 
-    /**
-     * @constructor
-     * @extends {Glass}
-     */
-    function Pint() {
-        Glass.call(this, Glass.Size.PINT);
-        this.quantity = Glass.Size.PINT;
-    }
+	/**
+	 * @constructor
+	 * @extends {Glass}
+	 */
+	function Pint() {
+		Glass.call(this, Glass.Size.PINT);
+		this.quantity = Glass.Size.PINT;
+	}
 
-    Pint.prototype = Object.create(Glass.prototype);
-    Pint.prototype.constructor = Pint;
+	Pint.prototype = Object.create(Glass.prototype);
+	Pint.prototype.constructor = Pint;
 
-    return Pint;
+	return Pint;
 });
 /*global define */
 
 define('pub/halfPint',['pub/glass'], function (Glass) {
-    
+	
 
-    /**
-     * @constructor
-     * @extends {Glass}
-     */
-    function HalfPint() {
-        Glass.call(this, Glass.Size.HALF_PINT);
-        this.quantity = Glass.Size.HALF_PINT;
-        this.test = 'test';
-    }
+	/**
+	 * @constructor
+	 * @extends {Glass}
+	 */
+	function HalfPint() {
+		Glass.call(this, Glass.Size.HALF_PINT);
+		this.quantity = Glass.Size.HALF_PINT;
+		this.test = 'test';
+	}
 
-    HalfPint.prototype = Object.create(Glass.prototype);
-    HalfPint.prototype.constructor = HalfPint;
+	HalfPint.prototype = Object.create(Glass.prototype);
+	HalfPint.prototype.constructor = HalfPint;
 
-    return HalfPint;
+	return HalfPint;
 });
 /*global define */
 
 define('pub/jug',['pub/glass'], function (Glass) {
-    
+	
 
-    /**
-     * @constructor
-     * @extends {Glass}
-     */
-    function Jug(){
-        Glass.call(this, Glass.Size.JUG);
-        this.quantity = Glass.Size.JUG;
-    }
+	/**
+	 * @constructor
+	 * @extends {Glass}
+	 */
+	function Jug() {
+		Glass.call(this, Glass.Size.JUG);
+		this.quantity = Glass.Size.JUG;
+	}
 
-    Jug.prototype = Object.create(Glass.prototype);
-    Jug.prototype.constructor = Jug;
+	Jug.prototype = Object.create(Glass.prototype);
+	Jug.prototype.constructor = Jug;
 
-    return Jug;
+	return Jug;
 });
 /*global define */
 
 define('pub/customer',[], function () {
-    
+	
 
-    /** @constructor */
-    function Customer() {
+	/** @constructor */
+	function Customer() {
 
-    }
+	}
 
-    /** @param {Glass} glass */
-    Customer.prototype.drink = function (glass) {
-        glass.consume(1);
-    };
-    /** @param {Glass} glass */
-    Customer.prototype.quaff = function (glass) {
-        glass.consume(4);
-    };
-    /** @param {Glass} glass */
-    Customer.prototype.downInOne = function (glass) {
-        glass.consume(glass.quantity);
-    };
+	/** @param {Glass} glass */
+	Customer.prototype.drink = function (glass) {
+		glass.consume(1);
+	};
+	/** @param {Glass} glass */
+	Customer.prototype.quaff = function (glass) {
+		glass.consume(4);
+	};
+	/** @param {Glass} glass */
+	Customer.prototype.downInOne = function (glass) {
+		glass.consume(glass.quantity);
+	};
 
-    return Customer;
+	return Customer;
 });
 /*global define */
 
@@ -545,21 +547,21 @@ define('pub/customer',[], function () {
  * The main module that defines the public interface for pub, a made-up library.
  */
 define('pub',['require','pub/glass','pub/pint','pub/halfPint','pub/jug','pub/customer'],function (require) {
-    
+	
 
-    var Glass = require('pub/glass'),
-        Pint = require('pub/pint'),
-        HalfPint = require('pub/halfPint'),
-        Jug = require('pub/jug'),
-        Customer = require('pub/customer');
+	var Glass = require('pub/glass'),
+		Pint = require('pub/pint'),
+		HalfPint = require('pub/halfPint'),
+		Jug = require('pub/jug'),
+		Customer = require('pub/customer');
 
-    window.pub = {
-        Glass : Glass,
-        Pint : Pint,
-        HalfPint : HalfPint,
-        Jug : Jug,
-        Customer : Customer
-    };
+	window.pub = {
+		Glass : Glass,
+		Pint : Pint,
+		HalfPint : HalfPint,
+		Jug : Jug,
+		Customer : Customer
+	};
 
 	return window.pub;
 });
--- dist/pub.spec.js
+++ dist/pub.spec.js
@@ -1,91 +1,134 @@
 /*global describe */
 
-describe('Pub', function() {
+describe('Pub', function () {
 	'use strict';
 
 });
 /*global pub, describe, it, beforeEach, expect */
 
-describe('Customer', function() {
+describe('Customer', function () {
 	'use strict';
 
-    var customer,
-        halfPint,
-        pint,
-        jug;
+	var customer,
+		halfPint,
+		pint,
+		jug;
 
-    beforeEach(function() {
-        customer = new pub.Customer();
-        halfPint = new pub.HalfPint();
-        pint = new pub.Pint();
-        jug = new pub.Jug();
-    });
+	beforeEach(function () {
+		customer = new pub.Customer();
+		halfPint = new pub.HalfPint();
+		pint = new pub.Pint();
+		jug = new pub.Jug();
+	});
 
-    it('Drinks, 1 fl. oz. is consumed', function() {
-        customer.drink(halfPint);
-        customer.drink(pint);
-        customer.drink(jug);
+	it('Drinks, 1 fl. oz. is consumed', function () {
+		customer.drink(halfPint);
+		customer.drink(pint);
+		customer.drink(jug);
 
-        expect(halfPint.quantity).toEqual(9);
-        expect(pint.quantity).toEqual(19);
-        expect(jug.quantity).toEqual(59);
-    });
+		expect(halfPint.quantity).toEqual(9);
+		expect(pint.quantity).toEqual(19);
+		expect(jug.quantity).toEqual(59);
+	});
 
-    it('Quaffs, 4 fl. oz. are consumed', function() {
-        customer.quaff(halfPint);
-        customer.quaff(pint);
-        customer.quaff(jug);
+	it('Quaffs, 4 fl. oz. are consumed', function () {
+		customer.quaff(halfPint);
+		customer.quaff(pint);
+		customer.quaff(jug);
 
-        expect(halfPint.quantity).toEqual(6);
-        expect(pint.quantity).toEqual(16);
-        expect(jug.quantity).toEqual(56);
-    });
+		expect(halfPint.quantity).toEqual(6);
+		expect(pint.quantity).toEqual(16);
+		expect(jug.quantity).toEqual(56);
+	});
 
-    it('Downs in one, the remaining beer is consumed', function(){
-        customer.downInOne(halfPint);
-        customer.downInOne(pint);
-        customer.downInOne(jug);
+	it('Downs in one, the remaining beer is consumed', function () {
+		customer.downInOne(halfPint);
+		customer.downInOne(pint);
+		customer.downInOne(jug);
 
-        expect(halfPint.quantity).toEqual(0);
-        expect(pint.quantity).toEqual(0);
-        expect(jug.quantity).toEqual(0);
-    });
+		expect(halfPint.quantity).toEqual(0);
+		expect(pint.quantity).toEqual(0);
+		expect(jug.quantity).toEqual(0);
+	});
+
+	it('Cannot drink from a beer that has already been consumed', function () {
+		customer.downInOne(halfPint);
+		customer.downInOne(pint);
+		customer.downInOne(jug);
+
+		customer.drink(halfPint);
+		customer.drink(pint);
+		customer.drink(jug);
+
+		expect(halfPint.quantity).toEqual(0);
+		expect(pint.quantity).toEqual(0);
+		expect(jug.quantity).toEqual(0);
+	});
+
+	it('Cannot quaff from a beer that has already been consumed', function () {
+		customer.downInOne(halfPint);
+		customer.downInOne(pint);
+		customer.downInOne(jug);
+
+		customer.quaff(halfPint);
+		customer.quaff(pint);
+		customer.quaff(jug);
+
+		expect(halfPint.quantity).toEqual(0);
+		expect(pint.quantity).toEqual(0);
+		expect(jug.quantity).toEqual(0);
+	});
+
+	it('Cannot down in one from a beer that has already been consumed', function () {
+		customer.downInOne(halfPint);
+		customer.downInOne(pint);
+		customer.downInOne(jug);
+
+		customer.downInOne(halfPint);
+		customer.downInOne(pint);
+		customer.downInOne(jug);
+
+		expect(halfPint.quantity).toEqual(0);
+		expect(pint.quantity).toEqual(0);
+		expect(jug.quantity).toEqual(0);
+	});
+
 });
 
 /*global pub, describe, it, beforeEach, expect */
-describe('Glass', function() {
+describe('Glass', function () {
 	'use strict';
 
-    var glass;
+	var glass;
 
-    beforeEach(function() {
-        glass = new pub.Glass();
-    });
+	beforeEach(function () {
+		glass = new pub.Glass();
+	});
 
-    it('Is empty', function() {
-        expect(glass.quantity).toEqual(0);
-    });
+	it('Is empty', function () {
+		expect(glass.quantity).toEqual(0);
+	});
 
 });
 
 /*global pub, describe, it, beforeEach, expect */
 
-describe('Half Pint', function() {
+describe('Half Pint', function () {
 	'use strict';
 
-    var halfPint;
+	var halfPint;
 
-    beforeEach(function() {
-        halfPint = new pub.HalfPint();
-    });
+	beforeEach(function () {
+		halfPint = new pub.HalfPint();
+	});
 
-    it('Contains 10 fl. oz. of beer', function () {
-        expect(halfPint.quantity).toEqual(10);
-    });
+	it('Contains 10 fl. oz. of beer', function () {
+		expect(halfPint.quantity).toEqual(10);
+	});
 
-    it('Has 10 fl. oz. of capacity', function () {
-        expect(halfPint.capacity).toEqual(10);
-    });
+	it('Has 10 fl. oz. of capacity', function () {
+		expect(halfPint.capacity).toEqual(10);
+	});
 
 });
 /*global pub, describe, it, beforeEach, expect */
@@ -93,38 +136,38 @@ describe('Half Pint', function() {
 describe('Jug', function () {
 	'use strict';
 
-    var jug;
+	var jug;
 
-    beforeEach(function () {
-        jug = new pub.Jug();
-    });
+	beforeEach(function () {
+		jug = new pub.Jug();
+	});
 
-    it('Contains 3 pints (60 fl. oz.) of beer', function () {
-        expect(jug.quantity).toEqual(60);
-    });
+	it('Contains 3 pints (60 fl. oz.) of beer', function () {
+		expect(jug.quantity).toEqual(60);
+	});
 
-    it('Has 3 pints (60 fl. oz.) of capacity', function () {
-        expect(jug.capacity).toEqual(60);
-    });
+	it('Has 3 pints (60 fl. oz.) of capacity', function () {
+		expect(jug.capacity).toEqual(60);
+	});
 
 });
 /*global pub, describe, it, beforeEach, expect */
 
-describe('Pint', function() {
+describe('Pint', function () {
 	'use strict';
 
-    var pint;
+	var pint;
 
-    beforeEach(function() {
-        pint = new pub.Pint();
-    });
+	beforeEach(function () {
+		pint = new pub.Pint();
+	});
 
-    it('Contains 20 fl. oz. of beer', function () {
-        expect(pint.quantity).toEqual(20);
-    });
+	it('Contains 20 fl. oz. of beer', function () {
+		expect(pint.quantity).toEqual(20);
+	});
 
-    it('Has 20 fl. oz. of capacity', function () {
-        expect(pint.capacity).toEqual(20);
-    });
+	it('Has 20 fl. oz. of capacity', function () {
+		expect(pint.capacity).toEqual(20);
+	});
 
 });
